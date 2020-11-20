2b12af09a5e7da0b96d1d1f22b37378682da08b2
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-11-20 11:31:17 +0100

    vendrediv1

---
+++ javascript/script.js
@@ -0,0 +1,44 @@
+function cache() {
+    if (document.getElementById('changer').innerHTML == 'vidéo') {
+        document.getElementById('changer').innerHTML = 'texte';
+        document.getElementById('intro').style.display = 'none';
+    } else {
+        document.getElementById('changer').innerHTML = 'vidéo'
+        document.getElementById('intro').style.display = 'block'
+    }
+}
+
+function affiche() {
+    if (document.getElementById('montrer').innerHTML == 'montrer') {
+        document.getElementById('montrer').innerHTML = 'cacher';
+        document.getElementById('article1').style.display = 'block';
+    } else {
+        document.getElementById('montrer').innerHTML = 'montrer'
+        document.getElementById('article1').style.display = 'none'
+    }
+}
+
+function affiche2() {
+    if (document.getElementById('montrer2').innerHTML == 'montrer') {
+        document.getElementById('montrer2').innerHTML = 'cacher';
+        document.getElementById('article2').style.display = 'block';
+    } else {
+        document.getElementById('montrer2').innerHTML = 'montrer'
+        document.getElementById('article2').style.display = 'none'
+    }
+}
+
+
+function intro() {
+    if (window.innerWidth < 1024 && window.innerWidth > 690) {
+        document.getElementById('intro').style.display = 'block'
+    }
+}
+
+function footer() {
+    if (window.innerheight > document.body.clientHeight) {
+        document.getElementsById('footeraccueril').style.position = 'fixed'
+    } else {
+        document.getElementsById('footeraccueil').style.position = 'absolute'
+    }
+}
