a383cce33c060924c5b8a6630e7d336d1b73d397
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-11-25 09:13:58 +0100

    mardi24last

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,35 @@
+$('#logoa').animate({
+    marginLeft: '500px'
+}, 1000)
+
+$('#logoa').animate({
+    marginLeft: '-250px'
+}, 800)
+
+$('#logoa').animate({
+    marginLeft: '0px'
+}, 500)
+
+
+
+$('#titrea').animate({
+    marginLeft: '-465px'
+}, 1000)
+
+$('#titrea').animate({
+    marginLeft: '285px'
+}, 800)
+
+$('#titrea').animate({
+    marginLeft: '35px'
+}, 500)
+
+
+
+
+
+
+
 function cache() {
     if (document.getElementById('changer').innerHTML == 'vidéo') {
         document.getElementById('changer').innerHTML = 'texte';
