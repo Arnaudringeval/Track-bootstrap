b02c46901fae01cc3f085d510f5b080a738f9760
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-12-01 00:20:20 +0100

    lundi3011v1

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,174 @@
-$('#logoa').animate({
-    marginLeft: '500px'
-}, 1000)
+if (window.innerWidth < 690) {
+    $('#logoa').animate({
+        marginLeft: '500px'
+    }, 1000)
 
-$('#logoa').animate({
-    marginLeft: '-250px'
-}, 800)
+    $('#logoa').animate({
+        marginLeft: '-250px'
+    }, 800)
 
-$('#logoa').animate({
-    marginLeft: '0px'
-}, 500)
+    $('#logoa').animate({
+        marginLeft: '0px'
+    }, 500)
 
 
 
-$('#titrea').animate({
-    marginLeft: '-465px'
-}, 1000)
+    $('#titrea').animate({
+        marginLeft: '-500px'
+    }, 1000)
 
-$('#titrea').animate({
-    marginLeft: '285px'
-}, 800)
+    $('#titrea').animate({
+        marginLeft: '250px'
+    }, 800)
 
-$('#titrea').animate({
-    marginLeft: '35px'
-}, 500)
+    $('#titrea').animate({
+        marginLeft: '0px'
+    }, 500)
+}
+
+
+if (window.innerWidth < 1024 && window.innerWidth > 690) {
+    $('#logoa').fadeOut(1000)
+
+    $('#logoa').fadeIn(250)
+
+    $('#titrea').animate({
+        fontSize: '67px'
+    }, 800)
+
+    $('#titrea').animate({
+        fontSize: '83px'
+    }, 600)
+
+    $('#titrea').animate({
+        fontSize: '67px'
+    }, 400)
+
+    $('#titrea').animate({
+        fontSize: '75px'
+    }, 200)
+
+    $('.namea').animate({
+        fontSize: '30px'
+    }, 800)
+
+    $('.namea').animate({
+        fontSize: '39px'
+    }, 600)
+
+    $('.namea').animate({
+        fontSize: '30px'
+    }, 400)
+
+    $('.namea').animate({
+        fontSize: '35px'
+    }, 200)
+
+
+}
+
 
 
 $('#dropback').click(function() {
     $('.navbar').toggleClass('couleur');
 })
+
+
+$(document).ready(function() {
+
+    var $pseudo = $('#pseudo'),
+        $mdp = $('#sujet'),
+        $mail = $('#mail'),
+        $envoi = $('#envoi'),
+        $message = $('#message'),
+        $erreur = $('#erreur'),
+        $champ = $('.champ');
+
+    $champ.keyup(function() {
+        if ($pseudo.val().length < 2) { // si la chaîne de caractères est inférieure à 5
+            $pseudo.css({ // on rend le champ rouge
+                backgroundColor: 'red',
+                color: 'black'
+            });
+        } else {
+            $pseudo.css({ // si tout est bon, on le rend vert
+                backgroundColor: 'white',
+                color: 'green'
+            });
+        }
+    });
+
+
+
+    $champ.keyup(function() {
+        if ($mdp.val().length < 2) { // si la chaîne de caractères est inférieure à 5
+            $mdp.css({ // on rend le champ rouge
+                backgroundColor: 'red',
+                color: 'black'
+            });
+        } else {
+            $mdp.css({ // si tout est bon, on le rend vert
+                backgroundColor: 'white',
+                color: 'green'
+            });
+        }
+    });
+
+
+    $champ.keyup(function() {
+        if ($mail.val().length < 2) { // si la chaîne de caractères est inférieure à 5
+            $mail.css({ // on rend le champ rouge
+                backgroundColor: 'red',
+                color: 'black'
+            });
+        } else {
+            $mail.css({ // si tout est bon, on le rend vert
+                backgroundColor: 'white',
+                color: 'green'
+            });
+        }
+    });
+
+    $champ.keyup(function() {
+        if ($message.val().length < 2) { // si la chaîne de caractères est inférieure à 5
+            $message.css({ // on rend le champ rouge
+                backgroundColor: 'red',
+                color: 'black'
+            });
+        } else {
+            $message.css({ // si tout est bon, on le rend vert
+                backgroundColor: 'white',
+                color: 'green'
+            });
+        }
+    });
+
+
+    $envoi.click(function(e) {
+        e.preventDefault(); // on annule la fonction par défaut du bouton d'envoi
+
+        // puis on lance la fonction de vérification sur tous les champs :
+        verifier($pseudo);
+        verifier($mdp);
+        verifier($message);
+        verifier($mail);
+    });
+
+
+
+    function verifier(champ) {
+        if (champ.val().length < 2) { // si le champ est vide
+            $erreur.css('display', 'block'); // on affiche le message d'erreur
+            champ.css({ // on rend le champ rouge
+                backgroundColor: 'red',
+                color: 'red'
+            });
+        }
+    }
+
+});
+
+
 
 
 
